3ef47851aa0eeecd632f3e2f2dd3125f04ceef5b
Руньковский Константин <raitok@mail.ru>
2021-05-31 19:55:30 +0300

    refactor(script.js): delete html

--- script/script.js
+++ script/script.js
@@ -2,37 +2,28 @@
 
 // 1 задание
 
-const langForm = document.querySelector('.lang-form');
-const ifOutput = document.querySelector('.if-output');
-const switchCaseOutput = document.querySelector('.switch-case-output');
-const arrayOutput = document.querySelector('.array-output');
-let days = new Map([
+const days = new Map([
     ['ru', ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']],
     ['en', ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']]
 ]);
+const lang = prompt('Введите язык (ru или en)');
 
-langForm.addEventListener('submit', function(evt) {
-    evt.preventDefault();
-    const lang = document.querySelector('input[name="lang"]:checked').value;
-    console.log(lang);
+if (lang === 'ru') {
+    console.log(days.get('ru'));
+} else {
+    console.log(days.get('en'));
+}
 
-    if (lang === 'ru') {
-        ifOutput.textContent = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье';        
-    } else {
-        ifOutput.textContent = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday';  
-    }
+switch (lang) {
+    case 'ru':
+        console.log(days.get('ru'));          
+        break;
 
-    switch (lang) {
-        case 'ru':
-            switchCaseOutput.textContent = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье';            
-            break;
-    
-        case 'en':
-            switchCaseOutput.textContent = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday';
-            break;
-    }
-    arrayOutput.textContent = days.get(lang);
-});
+    case 'en':
+        console.log(days.get('en'));
+        break;
+}
+console.log(days.get(lang));
 
 // 2 задание
 
