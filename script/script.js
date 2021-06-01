0d61cac9f0bbf09bdaf53c22569530fd012b5c24
Руньковский Константин <raitok@mail.ru>
2021-06-01 20:44:37 +0300

    feat(script.js): add number check

--- script/script.js
+++ script/script.js
@@ -1,8 +1,14 @@
 'use strict';
 
+const isNumber = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
 const foo = function (str) {
-    if (typeof str !== 'string') {
-        console.log('Not a string');
+    if (isNumber(str)) {
+        console.log(`It's a number ${str}`);                
+    } else if (typeof str !== 'string') {
+        console.log('Not a string or number');
     } else {
         let trimedStr = str.trim();
         if (trimedStr.length > 30) {
@@ -13,4 +19,4 @@ const foo = function (str) {
     }
 };
 
-foo('         asdasd   asd sdklasdjkh asdjk ldhajksdh sajkdhjkd ahdkjads asd    ');
+foo('asdasd asd ads dda dsada dadss ad as dsa ddas dasd asd asd da  a   ');
