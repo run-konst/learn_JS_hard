5cb361cd5414d2f96d8e4bd02a7e1c199b41845e
Руньковский Константин <raitok@mail.ru>
2021-06-12 23:54:40 +0300

    feat(project) new feat

--- script/script.js
+++ script/script.js
@@ -1,61 +1,91 @@
 'use strict';
 
-const DomElement = function (selector, height, width, bg, fontSize) {    
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
-};
+let users = [];
 
-DomElement.prototype.create = function () {
-    if (this.selector[0] === '.') {
-        const newElement = document.createElement('div');
-        newElement.classList.add(this.selector.substr(1));
-        newElement.textContent = 'Привет, я Div c классом ' + this.selector.substr(1);
-        this.addStyle(newElement);
-        document.body.append(newElement);        
-    } else if (this.selector[0] === '#') {
-        const newElement = document.createElement('p');
-        newElement.id = this.selector.substr(1);
-        newElement.textContent = 'Привет, я параграф с ID ' + this.selector.substr(1);
-        this.addStyle(newElement);
-        document.body.append(newElement);      
+const 
+    username = document.querySelector('.username'),
+    regBtn = document.querySelector('.reg-btn'),
+    authBtn = document.querySelector('.auth-btn'),
+    userList = document.querySelector('.user-list'),
+    options = {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+    },
+    capitalizeFirstLetter = function (arr) {
+        let newArr = [];
+        arr.forEach( function (element) {
+            newArr.push(element.charAt(0).toUpperCase() + element.slice(1));       
+        });
+        return newArr;
+    },
+    saveStorage = function () {
+        localStorage.removeItem('users');
+        localStorage.setItem('users', JSON.stringify(users));
+    },
+    loadStorage = function () {
+        if (localStorage.getItem('users')) {
+            users = JSON.parse(localStorage.getItem('users'));
+        } else {
+            users = [];
+        }
+    },
+    render = function () {
+        userList.textContent = '';
+
+        loadStorage();
+
+        users.forEach(function (item) {
+            const li = document.createElement('li');
+            li.innerHTML = `Имя: ${item.name}, Фамилия: ${item.surname}, зарегистрирован: ${item.regDate}<button class="delete-btn">Удалить</button>`;
+            userList.append(li);
+            const deleteBtn = li.querySelector('.delete-btn');
+            deleteBtn.addEventListener('click', function () {
+                users.splice(users.indexOf(item), 1);
+                saveStorage();
+                render();    
+            });
+        });
+    };
+
+regBtn.addEventListener('click', function () {
+    let newUser = {};
+    let user = prompt('Введите ваши имя и фамилию через пробел');
+    user = user.trim().toLocaleLowerCase().split(' ');
+    if (user.length !== 2) {
+        alert('Ошибка ввода');
+        return;        
     }
-};
-DomElement.prototype.addStyle = function (elem) {
-    elem.style.cssText = `
-    height: ${this.height};
-    width: ${this.width};
-    background-color: ${this.bg};
-    font-size: ${this.fontSize};
-    `;     
-};
-
-const elem = new DomElement('#block', '100px', '300px', 'yellow', '30px');
-elem.create();
-
-const squareElem = new DomElement('.square', '100px', '100px', 'red', '16px');
-
-document.addEventListener("DOMContentLoaded", squareElem.create());
-
-const square = document.querySelector('.square');
-
-square.style.position = 'absolute';
-square.style.top = 0;
-square.style.left = 0;
-
-document.addEventListener('keydown', function (event) {
-    if (event.keyCode === 38) {
-        square.style.top = parseInt(square.style.top) - 10 + 'px';        
-    } //up
-    if (event.keyCode === 40) {
-        square.style.top = parseInt(square.style.top) + 10 + 'px';     
-    } //down
-    if (event.keyCode === 37) {
-        square.style.left = parseInt(square.style.left) - 10 + 'px';        
-    } //left
-    if (event.keyCode === 39) {
-        square.style.left = parseInt(square.style.left) + 10 + 'px';        
-    } //right
+    user = capitalizeFirstLetter(user);
+    newUser.name = user[0];
+    newUser.surname = user[1];
+    newUser.login = prompt('Введите ваш логин');
+    newUser.password = prompt('Введите пароль');
+    newUser.regDate = new Date().toLocaleString('ru-RU', options);
+    users.push(newUser);
+    saveStorage();
+    render(); 
 });
+
+authBtn.addEventListener('click', function () {
+    const login = prompt('Введите логин');
+    const password = prompt('Введите пароль');
+    let logged = false;
+    for (let i = 0; i < users.length; i++) {
+        const user = users[i];
+        if (user.login === login && user.password === password) {
+            username.textContent = user.name;
+            localStorage.setItem('username', user.name);
+            logged = true;
+        }
+    }
+    if (logged === false) {
+        alert('Пользователь не найден');        
+    }
+});
+
+username.textContent = localStorage.getItem('username');
+render();
