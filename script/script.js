3699945885be3b5ddd554e2e1d84fa241a9d657c
Руньковский Константин <raitok@mail.ru>
2021-06-18 22:37:54 +0300

    feat(project) animation frame

--- script/script.js
+++ script/script.js
@@ -1,18 +1,42 @@
 'use strict';
 
+let 
+    count = 0,
+    animationFrame,
+    animation = false;
+
 const
-    input = document.querySelector('input'),
-    text = document.querySelector('p'),
-    handleEvent = () => text.textContent = input.value,
-    debounce = (func, time) => {
-        return function (args) {
-            let previousCall = this.lastCall;
-            this.lastCall = Date.now();
-            if (previousCall && ((this.lastCall - previousCall) <= time)) {
-                clearTimeout(this.lastCallTimer);
-            }
-            this.lastCallTimer = setTimeout(() => func(args), time);
-        };
+    square = document.querySelector('.square'),
+    circle = document.querySelector('.circle'),
+    start = document.querySelector('.start-btn'),
+    reset = document.querySelector('.reset-btn'),
+    animate = () => {
+        animationFrame = requestAnimationFrame(animate);
+        count++;
+        if (count < 100) {
+            square.style.top = count*2 + 'px';
+        }
+        if (count < 200) {
+            circle.style.top = count*2 + 'px';
+        } else {
+            cancelAnimationFrame(animationFrame);
+        }
     };
-    
-input.addEventListener('input', debounce(handleEvent, 300));
+
+start.addEventListener('click', () => {
+    if (animation) {
+        cancelAnimationFrame(animationFrame);
+        animation = false;     
+    } else {
+        animationFrame = requestAnimationFrame(animate);
+        animation = true; 
+    }
+});
+
+reset.addEventListener('click', () => {
+    cancelAnimationFrame(animationFrame);
+    count = 0;
+    square.style.top = 0;
+    circle.style.top = 0;
+    animation = false;    
+});
