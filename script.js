a0a9e6c0292cd4025879534844073bf38a3c9a6b
Руньковский Константин <raitok@mail.ru>
2021-07-04 23:40:26 +0300

    feat(project) fetch money rates

--- script.js
+++ script.js
@@ -2,44 +2,39 @@ document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     const
-        select = document.getElementById('cars'),
-        output = document.getElementById('output'),
-        getCar = () => {
-            return new Promise((resolve, reject) => {
-                const request = new XMLHttpRequest();
-                request.addEventListener('readystatechange', () => {
-                    if (request.readyState !== 4) {
-                        return;
-                    }
-                    if (request.status === 200) {
-                        resolve(JSON.parse(request.responseText));
-                    } else {
-                        reject(request.status);
-                    }
-                });
-                request.open('GET', './cars.json');
-                request.setRequestHeader('Content-type', 'application/json');
-                request.send();
-            });
+        valInput = document.querySelector('.val-input'),
+        rubToValSelect = document.querySelector('.rub-to-val-select'),
+        rubResult = document.querySelector('.rub-result'),
+        valBtn = document.querySelector('.val-btn'),
+        rubInput = document.querySelector('.rub-input'),
+        valToRubSelect = document.querySelector('.val-to-rub-select'),
+        valResult = document.querySelector('.val-result'),
+        rubBtn = document.querySelector('.rub-btn'),
+        rates = {},
+        getRates = (data) => {
+            rates.EUR = data.rates.RUB;
+            rates.USD = data.rates.RUB/data.rates.USD;
+            },
+        calculateVal = () => {
+            rubResult.value = Math.round(valInput.value * rates[rubToValSelect.value] * 100) / 100;
         },
-        showCar = (data) => {
-            data.cars.forEach(item => {
-                if (item.brand === select.value) {
-                    const {brand, model, price} = item;
-                    output.innerHTML = `Тачка ${brand} ${model} <br>
-                    Цена: ${price}$`;
-                }
-            });
-        },        
-        showError = (error) => {
-            output.innerHTML = 'Произошла ошибка';
-            console.error(error);
+        calculateRub = () => {
+            valResult.value = Math.round(rubInput.value / rates[valToRubSelect.value] * 100) / 100;
         };
 
-    select.addEventListener('change', () => {
-        getCar()
-            .then(showCar)
-            .catch(showError);
-    });
+    fetch('http://api.exchangeratesapi.io/v1/latest?access_key=5152f4e656ceaf0d38358bc090f7cbe9')
+        .then(response => {    
+            if (response.status !== 200) {
+                throw new Error('Network status is not 200');
+            }
+            return response.json();
+        })
+        .then(getRates)
+        .catch(error => {
+            console.error(error);
+        });
+
+    valBtn.addEventListener('click', calculateVal);
+    rubBtn.addEventListener('click', calculateRub);
 
 });
