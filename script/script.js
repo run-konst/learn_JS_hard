7b8507d8320fee9b6b409c5cd670a8bec5c057a4
Руньковский Константин <raitok@mail.ru>
2021-07-04 21:05:51 +0300

    regexp

--- script/script.js
+++ script/script.js
@@ -1,69 +1,37 @@
 'use strict';
 
-const
-    sumBtn = document.querySelector('#sum'),
-    multBtn = document.querySelector('#mult'),
-    res = document.querySelector('#res'),
-    calculator = {
-        a: 0,
-        b: 0,
-        c: 0,
-        getValues() {
-            this.a = +document.querySelector('#a').value;
-            this.b = +document.querySelector('#b').value;
-        },
-        sum() {
-            this.getValues();
-            this.c =  this.a + this.b;
-            this.show();
-        },
-        mult() {
-            this.getValues();
-            this.c = this.a * this.b;
-            this.show();
-        },
-        show() {
-            res.value = this.c;
-        },
-        eventListeners() {
-            sumBtn.addEventListener('click', this.sum.bind(this));
-            multBtn.addEventListener('click', this.mult.bind(this));
-        }
-    };
+const task1 = document.getElementById('task1');
+task1.innerHTML = task1.innerHTML.replace(/функци.+?(?=[^а-я])/gi, '<strong>$&</strong>');
 
-calculator.eventListeners();
+const task2 = document.getElementById('task2');
+task2.innerHTML = task2.innerHTML.replace(/(<b>)\d\d:\d\d.?(<\/b>)/gi, match => match.substring(3, match.length - 4));
+task2.innerHTML = task2.innerHTML.replace(/\d\d:\d\d/gi, '<b>$&</b>');
 
-const cityArr = {
-    rus: ['Москва', 'Санк-Петербург', 'Новосибирск', 'Екатеринбург', 'Нижний Новгород', 'Казань', 'Челябинск'],
-    uk: ['Киев', 'Харьков', 'Одесса', 'Днепр', 'Донецк', 'Запорожье', 'Львов'],
-    bel: ['Минск', 'Гомель', 'Могилёв', 'Витебск', 'Гродно', 'Брест'],
-    jap: ['Токио', 'Киото', 'Осака', 'Иокогама'] 
-};
+document.body.innerHTML = document.body.innerHTML.replace(/=?".+?"|«.+?»/g, match => match.match(/=".+?"/) ? match : `<mark>${match}</mark>`);
 
-const 
-    countrySelect = document.querySelector('#country'),
-    citySelect = document.querySelector('#city'),
-    result = document.querySelector('.result'),
-    countryOptions = countrySelect.querySelectorAll('option'),
-    countryArr = {};
+document.body.innerHTML = document.body.innerHTML.replace(/http:.+?ru/g, match => `<a href="${match}">${match.substr(7)}</a>`);
 
-countryOptions.forEach(item => {
-    countryArr[item.value] = item.textContent;
-});
+console.log(document.body.innerHTML.match(/#334455/));
 
-countrySelect.addEventListener('change', () => {
-    citySelect.style.display = 'inline-block';
-    citySelect.textContent = '';
-    const country = countrySelect.value;
-    const cities = cityArr[country];
-    cities.forEach(item => {
-        const city = document.createElement('option');
-        city.value = item;
-        city.textContent = item;
-        citySelect.append(city);
-    });
-});
+document.body.innerHTML = document.body.innerHTML.replace(/http:.+?html/g, match => `<a href="${match}">${match.substr(7).replace(/\/.+?html/, '')}</a>`);
 
-citySelect.addEventListener('change', () => {
-    result.textContent = `${countryArr[countrySelect.value]}, ${citySelect.value}`;
-});
+/* 
+
+1.Написать скрипт, которые заменяет слово "функция" и его однокоренные слова в div с id=task1 на «<strong>функция</strong>». 
+
+2. Написать скрипт который в div с id=task2 найдет время. Время имеет формат часы:минуты. И часы, и минуты состоят из двух цифр, пример: 09:00.
+заключить найденное время в тег <b></b>
+
+3. Создать запрос во всем документе найти текст в кавычках и заключить его в теги <mark></mark>
+
+4. Замените в документе домены вида http://site.ru на <a href="http://site.ru">site.ru</a>,
+
+5. Напишите регулярное выражение для поиска цвета, заданного как #ABCDEF, вывести цвет в консоль
+
+6. Ссылки такого вида http://site.ru/aaaa/bbbb.html заменить
+на <a href="http://site.ru/aaaa/bbbb.html">site.ru</a>
+
+
+Попрактикуйтесь на кроссвордах https://regexcrossword.com/
+и на задачках https://habr.com/ru/post/167015/
+ */
